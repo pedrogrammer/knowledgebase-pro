536b80c786f53e9d03bc35341711cd892e0dd945
p.sheikhrahimi <pedramshrahimi@gmail.com>
2026-02-08 19:43:48 +0330

    day 2

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Login | KnowledgeBase Pro",
+  description: "Login to access the knowledge base",
+};
+
 const LoginPage = () => {
   return <div>LoginPage</div>;
 };
